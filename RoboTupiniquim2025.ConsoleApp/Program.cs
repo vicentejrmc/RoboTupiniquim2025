73ea3f7f5e4b4fe86deaf3f6185b9d9bebc9ba02
vicentejrmc <vicente_pc@outlook.com>
2025-03-27 18:09:07 -0300

    Analise do projeto- detalhamento de Tarefas.

--- RoboTupiniquim2025.ConsoleApp/Program.cs
+++ RoboTupiniquim2025.ConsoleApp/Program.cs
@@ -1,14 +1,30 @@
-﻿namespace RoboTupiniquim2025.ConsoleApp
+﻿using System.Diagnostics.Metrics;
+using System;
+
+namespace RoboTupiniquim2025.ConsoleApp
 {
     internal class Program
     {
+        //Requisitos para implementação:
+        //---------------------------------------------------------    
+        //Leitura das coordenadas do canto superior direito da área.
+        //Ler a posição inicial do robô.
+        //Ler os comandos de direcionamento dados ao robô.
+        //Processar os comandos de direcionamento dados ao robô.
+        //Mover o robô de acordo às coordenadas de entrada. (../Leitura e entrada:/2.b:)
+        //a: Criar métodos para movimentação do robô. (Direita/Esquerda 90°)
+        //b: Criar método para mover o robô conforme(Direção/Mover) ((x1, y0 = x, y+1) == x1, y1.)
+        //Exibir a posição final do robô.
+
+
         static void Main(string[] args)
         {
-           //Robo Tupiniquim 2025
+
 
 
 
             Console.ReadLine();
         }
+
     }
 }
