eef48c876e6963c78821d85fd9ab0836cd49f8dd
vicentejrmc <vicente_pc@outlook.com>
2025-03-27 16:43:36 -0300

    Add project files.

---
+++ RoboTupiniquim2025.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace RoboTupiniquim2025.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Robo Tupiniquim 2025
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
