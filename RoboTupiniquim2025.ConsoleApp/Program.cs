9c7e26c20cd32f9e6bc611ac62a5a6ac30f0eefc
vicentejrmc <vicente_pc@outlook.com>
2025-03-30 18:12:05 -0300

    Implementada Estrutura de repetição while

--- RoboTupiniquim2025.ConsoleApp/Program.cs
+++ RoboTupiniquim2025.ConsoleApp/Program.cs
@@ -15,118 +15,132 @@ namespace RoboTupiniquim2025.ConsoleApp
         {
             ExibirMenu();
 
-            //Leitura das coordenadas do canto superior direito da área.
-            Console.Write("Canto Superior Direito:");
-            string[] cantoSuperiorDi = Console.ReadLine().Split(" ");
-            cantoSuperiorDi.ToArray();
-
-            int[] grid = new int[cantoSuperiorDi.Length];
-            for (int i = 0; i < cantoSuperiorDi.Length; i++)
-                grid[i] = int.Parse(cantoSuperiorDi[i]);
-
-            eixoX = grid[0];
-            eixoY = grid[1];
-
-            // posicão incial do Robô01
-            Console.Write("Posicao inicial Robo01: ");
-            string[] posIncialRo01 = Console.ReadLine().ToUpper().Split(" ");
-
-            int[] posRobo01 = new int[posIncialRo01.Length - 1];
-            for (int i = 0; i < posIncialRo01.Length - 1; i++)
-                posRobo01[i] = int.Parse(posIncialRo01[i]);
-
-            posR01X = posRobo01[0];
-            posR01Y = posRobo01[1];
-
-            direcaoAtR01 = char.Parse(posIncialRo01[2]);
-
-            //Ler os comandos de direcionamento dados ao Robô01.
-            Console.Write("Enviar Comandos Robo01: ");
-            string comandosRobo01 = Console.ReadLine().ToUpper();
-            comandosRobo01.ToArray();
-
-            char[] processarComandosR01 = new char[comandosRobo01.Length];
-
-            for (int i = 0; i < comandosRobo01.Length; i++)
-                processarComandosR01[i] = (comandosRobo01[i]);
-
-            for (int i = 0; i < processarComandosR01.Length; i++)
+            // Estrutura de Repetição. 
+            while (true)
             {
-                if (processarComandosR01[i] == 'E')
-                    direcaoAtR01 = GirarEsquerda(direcaoAtR01);
-                else if (processarComandosR01[i] == 'D')
-                    direcaoAtR01 = GirarDireita(direcaoAtR01);
-                else if (processarComandosR01[i] == 'M')
+                //Leitura das coordenadas do canto superior direito da área.
+                Console.Write("Canto Superior Direito: ");
+                string[] cantoSuperiorDi = Console.ReadLine().Split(" ");
+                cantoSuperiorDi.ToArray();
+
+                int[] grid = new int[cantoSuperiorDi.Length];
+                for (int i = 0; i < cantoSuperiorDi.Length; i++)
+                    grid[i] = int.Parse(cantoSuperiorDi[i]);
+
+                eixoX = grid[0];
+                eixoY = grid[1];
+
+                // posicão incial do Robô01
+                Console.Write("Posicao inicial Robo01: ");
+                string[] posIncialRo01 = Console.ReadLine().ToUpper().Split(" ");
+
+                int[] posRobo01 = new int[posIncialRo01.Length - 1];
+                for (int i = 0; i < posIncialRo01.Length - 1; i++)
+                    posRobo01[i] = int.Parse(posIncialRo01[i]);
+
+                posR01X = posRobo01[0];
+                posR01Y = posRobo01[1];
+
+                direcaoAtR01 = char.Parse(posIncialRo01[2]);
+
+                //Ler os comandos de direcionamento dados ao Robô01.
+                Console.Write("Enviar Comandos Robo01: ");
+                string comandosRobo01 = Console.ReadLine().ToUpper();
+                comandosRobo01.ToArray();
+
+                char[] processarComandosR01 = new char[comandosRobo01.Length];
+
+                for (int i = 0; i < comandosRobo01.Length; i++)
+                    processarComandosR01[i] = (comandosRobo01[i]);
+
+                for (int i = 0; i < processarComandosR01.Length; i++)
                 {
-                    switch (direcaoAtR01)
+                    if (processarComandosR01[i] == 'E')
+                        direcaoAtR01 = GirarEsquerda(direcaoAtR01);
+                    else if (processarComandosR01[i] == 'D')
+                        direcaoAtR01 = GirarDireita(direcaoAtR01);
+                    else if (processarComandosR01[i] == 'M')
                     {
-                        case 'N':
-                            posR01Y++;
-                            break;
-                        case 'S':
-                            posR01Y--;
-                            break;
-                        case 'O':
-                            posR01X--;
-                            break;
-                        case 'L':
-                            posR01X++;
-                            break;
+                        switch (direcaoAtR01)
+                        {
+                            case 'N':
+                                posR01Y++;
+                                break;
+                            case 'S':
+                                posR01Y--;
+                                break;
+                            case 'O':
+                                posR01X--;
+                                break;
+                            case 'L':
+                                posR01X++;
+                                break;
+                        }
                     }
                 }
-            }
 
-            // Posicao inicial Robô02
-            Console.Write("Posicao inicial Robo02: ");
-            string[] posIncialRo02 = Console.ReadLine().ToUpper().Split(" ");
+                // Posicao inicial Robô02
+                Console.Write("Posicao inicial Robo02: ");
+                string[] posIncialRo02 = Console.ReadLine().ToUpper().Split(" ");
 
-            int[] posRobo02 = new int[posIncialRo02.Length - 1];
-            for (int i = 0; i < posIncialRo02.Length - 1; i++)
-                posRobo02[i] = int.Parse(posIncialRo02[i]);
+                int[] posRobo02 = new int[posIncialRo02.Length - 1];
+                for (int i = 0; i < posIncialRo02.Length - 1; i++)
+                    posRobo02[i] = int.Parse(posIncialRo02[i]);
 
-            posR02X = posRobo02[0];
-            posR02Y = posRobo02[1];
-            direcaoAtR02 = char.Parse(posIncialRo02[2]);
+                posR02X = posRobo02[0];
+                posR02Y = posRobo02[1];
+                direcaoAtR02 = char.Parse(posIncialRo02[2]);
 
-            //Ler os comandos de direcionamento dados ao Robô02.
-            Console.Write("Enviar Comandos Robo02: ");
-            string comandosRobo02 = Console.ReadLine().ToUpper();
-            comandosRobo01.ToArray();
+                //Ler os comandos de direcionamento dados ao Robô02.
+                Console.Write("Enviar Comandos Robo02: ");
+                string comandosRobo02 = Console.ReadLine().ToUpper();
+                comandosRobo01.ToArray();
 
-            char[] processarComandosR02 = new char[comandosRobo02.Length];
+                char[] processarComandosR02 = new char[comandosRobo02.Length];
 
-            for (int i = 0; i < comandosRobo02.Length; i++)
-                processarComandosR02[i] = (comandosRobo02[i]);
+                for (int i = 0; i < comandosRobo02.Length; i++)
+                    processarComandosR02[i] = (comandosRobo02[i]);
 
-            for (int i = 0; i < processarComandosR02.Length; i++)
-            {
-                if (processarComandosR02[i] == 'E')
-                    direcaoAtR02 = GirarEsquerda(direcaoAtR02);
-                else if (processarComandosR02[i] == 'D')
-                    direcaoAtR02 = GirarDireita(direcaoAtR02);
-                else if (processarComandosR02[i] == 'M')
+                for (int i = 0; i < processarComandosR02.Length; i++)
                 {
-                    switch (direcaoAtR02)
+                    if (processarComandosR02[i] == 'E')
+                        direcaoAtR02 = GirarEsquerda(direcaoAtR02);
+                    else if (processarComandosR02[i] == 'D')
+                        direcaoAtR02 = GirarDireita(direcaoAtR02);
+                    else if (processarComandosR02[i] == 'M')
                     {
-                        case 'N':
-                            posR02Y++;
-                            break;
-                        case 'S':
-                            posR02Y--;
-                            break;
-                        case 'O':
-                            posR02X--;
-                            break;
-                        case 'L':
-                            posR02X++;
-                            break;
+                        switch (direcaoAtR02)
+                        {
+                            case 'N':
+                                posR02Y++;
+                                break;
+                            case 'S':
+                                posR02Y--;
+                                break;
+                            case 'O':
+                                posR02X--;
+                                break;
+                            case 'L':
+                                posR02X++;
+                                break;
+                        }
                     }
                 }
+
+                ExibirResultado();
+
+                Console.WriteLine("--------------------------------");
+                Console.Write("Deseja verifica nova Área? [S/N]: ");
+                string novaArea = Console.ReadLine().ToUpper();
+
+                if (novaArea != "S")
+                    break;
             }
 
-            ExibirResultado();
+            Console.WriteLine("Os Robôs Retornarão a base para recarregar.");
+            Console.Write("Pressione [Enter] para sair.");
+            Console.ReadKey();
 
-            Console.ReadLine();
         }
         public static char GirarEsquerda(char direcaoAt)
         {
